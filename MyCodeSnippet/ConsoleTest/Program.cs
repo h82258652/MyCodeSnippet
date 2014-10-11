0db3564349f68bd6f030c07ed51aa73836f29d27
842053625@qq.com <842053625>
2014-10-12 01:28:24 +0800

    update

---
+++ MyCodeSnippet/ConsoleTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCodeSnippet.JsonCharSerializer j = new MyCodeSnippet.JsonCharSerializer();
+            for (int i = 0; i < (int)char.MaxValue; i++)
+            {
+                var c = (char)i;
+                var my = j.Serializer(c);
+                var newt = Newtonsoft.Json.JsonConvert.SerializeObject(c);
+                var b = my == newt;
+                if (b == false)
+                {
+                    Console.WriteLine(c);
+                    break;
+                }
+            }
+            Console.WriteLine("finish");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyCodeSnippet/MyCodeSnippet/JsonCharSerializer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCodeSnippet
+{
+    public class JsonCharSerializer
+    {
+        public string Serializer(char c)
+        {
+            switch (c)
+            {
+                case '\b':// 8
+                    {
+                        return "\"\\b\"";
+                    }
+                case '\t':// 9
+                    {
+                        return "\"\\t\"";
+                    }
+                case '\n':// 10
+                    {
+                        return "\"\\n\"";
+                    }
+                case '\f':// 12
+                    {
+                        return "\"\\f\"";
+                    }
+                case '\r':// 13
+                    {
+                        return "\"\\r\"";
+                    }
+                case '\"':// 34
+                    {
+                        return "\"\\\"\"";
+                    }
+                case '\\':// 92
+                    {
+                        return "\"\\\\\"";
+                    }
+                default:
+                    {
+                        if (c < 0x20 || c == '\u0085' || c == '\u2028' || c == '\u2029')
+                        {
+                            return "\"\\u" + ((int)c).ToString("x4", CultureInfo.InvariantCulture) + "\"";
+                        }
+                        else
+                        {
+                            return "\"" + c + "\"";
+                        }
+                    }
+            }
+        }
+    }
+}
