9bed3f1c56c3c342c8c3b6d781ad0fadad237ef0
h82258652 <842053625@qq.com>
2014-10-12 15:55:46 +0800

    update

---
+++ MyCodeSnippet/MyCodeSnippet/JsonBooleanSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCodeSnippet
+{
+    public class JsonBooleanSerializer
+    {
+        public string Serialize(bool b)
+        {
+            return b ? "true" : "false";
+        }
+    }
+}
