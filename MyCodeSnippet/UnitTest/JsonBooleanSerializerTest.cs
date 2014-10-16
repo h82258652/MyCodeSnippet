7523f674cbc3644bd3b536e864ad0e9749bc6e1a
h82258652 <842053625@qq.com>
2014-10-16 12:03:27 +0800

    添加Boolean的序列化

---
+++ MyCodeSnippet/UnitTest/JsonBooleanSerializerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyCodeSnippet;
+using Newtonsoft.Json;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class JsonBooleanSerializerTest
+    {
+        [TestMethod]
+        public void SerializeTest()
+        {
+            JsonBooleanSerializer j = new JsonBooleanSerializer();
+            Assert.AreEqual(j.Serialize(true), JsonConvert.SerializeObject(true));
+            Assert.AreEqual(j.Serialize(false), JsonConvert.SerializeObject(false));
+        }
+    }
+}
