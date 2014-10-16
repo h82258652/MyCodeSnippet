b82d012e86fba66adc0b60a67785154512794bbc
h82258652 <842053625@qq.com>
2014-10-16 12:52:14 +0800

    Update

--- MyCodeSnippet/UnitTest/Json/JsonDoubleSerializerTest.cs
+++ MyCodeSnippet/UnitTest/Json/JsonDoubleSerializerTest.cs
@@ -5,6 +5,7 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyCodeSnippet.Json;
 using Newtonsoft.Json;
+using System.Web.Script.Serialization;
 
 namespace UnitTest.Json
 {
@@ -15,12 +16,25 @@ namespace UnitTest.Json
         public void SerializeTest()
         {
             JsonDoubleSerializer j = new JsonDoubleSerializer();
-            Assert.AreEqual(j.Serialize(double.MinValue), JsonConvert.SerializeObject(double.MinValue));
-            Assert.AreEqual(j.Serialize(double.MaxValue), JsonConvert.SerializeObject(double.MaxValue));
-            Assert.AreEqual(j.Serialize(0), JsonConvert.SerializeObject(0));
-            Assert.AreEqual(j.Serialize(Math.PI), JsonConvert.SerializeObject(Math.PI));
-            Assert.AreEqual(j.Serialize(Math.E), JsonConvert.SerializeObject(Math.E));
+            JavaScriptSerializer netj = new JavaScriptSerializer();
+
+            Assert.AreEqual(j.Serialize(0.5), netj.Serialize(0.5));
             Assert.AreEqual(j.Serialize(0.5), JsonConvert.SerializeObject(0.5));
+
+            Assert.AreEqual(j.Serialize(double.MinValue), JsonConvert.SerializeObject(double.MinValue));
+            Assert.AreEqual(j.Serialize(double.MinValue), netj.Serialize(double.MinValue));
+
+            Assert.AreEqual(j.Serialize(double.MaxValue), JsonConvert.SerializeObject(double.MaxValue));
+            Assert.AreEqual(j.Serialize(double.MaxValue), netj.Serialize(double.MaxValue));
+
+            Assert.AreEqual(j.Serialize(0), JsonConvert.SerializeObject(0));
+            Assert.AreEqual(j.Serialize(0), netj.Serialize(0));
+
+            Assert.AreEqual(j.Serialize(Math.PI), JsonConvert.SerializeObject(Math.PI));
+            Assert.AreEqual(j.Serialize(Math.PI), netj.Serialize(Math.PI));
+
+            Assert.AreEqual(j.Serialize(Math.E), JsonConvert.SerializeObject(Math.E));
+            Assert.AreEqual(j.Serialize(Math.E), netj.Serialize(Math.E));
         }
     }
 }
